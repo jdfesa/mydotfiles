21c04e5845984dabc58ad400d6736401cd742953
Jose David <asefadoj@gmail.com>
2026-01-19 20:36:21 -0300

    fix(ux): reposition mode indicator and robust triggers

--- sketchybar/plugins/mode.sh
+++ sketchybar/plugins/mode.sh
@@ -3,7 +3,7 @@
 # $CONFIG_DIR/plugins/mode.sh
 
 # The mode name is passed via the MODE environment variable from AeroSpace
-if [ "$MODE" = "" ]; then
+if [ "$MODE" = "" ] || [ "$MODE" = "NORMAL" ]; then
   sketchybar --set $NAME label="" drawing=off
 else
   sketchybar --set $NAME label="$MODE" drawing=on
